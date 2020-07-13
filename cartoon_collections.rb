13a7731c9f03149d535466705bab3b1d49185fc4
Elliott Stein <estein1988@gmail.com>
2020-07-13 18:29:48 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def greet_characters(array)
 end
 
 def list_dwarves(array)
-  array.each.with_index(1) do |index, dwarve|
+  array.each.with_index(1) do |dwarve, index|
     puts "#{dwarve} #{index}"
   end
 end
